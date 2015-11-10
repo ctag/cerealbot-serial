c5d9dcd9172ee6f4e62d6bf0123311c0015c0322
Christopher Bero <bigbero@gmail.com>
2015-11-10 07:36:25 -0600

    added request helper scripts

---
+++ request/lightOn.sh
@@ -0,0 +1,7 @@
+var request = require('request');
+
+request('http://localhost:3030/serialCmd?cmd=%5bsl1%5d', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        //console.log(body); // Show the HTML for the Modulus homepage.
+    }
+});
